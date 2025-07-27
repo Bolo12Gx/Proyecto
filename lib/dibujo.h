a1d8d82766865309948271c13a14ec5c16d1f4cb
Bolo12Gx <jorgeguala2006@gmail.com>
2025-07-26 22:16:57 -0500

    Documentacion de dibujo.h

--- lib/dibujo.h
+++ lib/dibujo.h
@@ -1,26 +1,40 @@
-#include <iostream>
-#include "../lib/color.h" 
-#include <ctime>
-
+#include <iostream>     // Para imprimir en consola
+#include "../lib/color.h" // Biblioteca personalizada para colores (se asume que define RESET)
+#include <ctime>        // No se usa aquí, pero probablemente se usa en otro archivo
 
 using namespace std;
 
+/**
+ * @brief Dibuja el estado actual del ahorcado en función de las vidas restantes.
+ * 
+ * @param vida Número de vidas restantes (de 6 a 0). Cada valor representa una etapa del dibujo.
+ * 
+ * El dibujo se va completando progresivamente:
+ * - 6 vidas: estructura vacía
+ * - 5 vidas: cabeza
+ * - 4 vidas: torso
+ * - 3 vidas: un brazo
+ * - 2 vidas: ambos brazos
+ * - 1 vida : una pierna
+ * - 0 vidas: figura completa (ahorcado)
+ */
 void ihDibujarAhorcado(int vida)
 {
-    
     switch(vida)
     {
         case 6:
+            // Estructura inicial sin figura
             cout << "  +---+\n"
                     "  |   |\n"
                     "      |\n"
                     "      |\n"
                     "      |\n"
                     "      |\n"
-                    "=========\n"<< RESET;
+                    "=========\n" << RESET;
+            break;
 
-            break;
         case 5:
+            // Se dibuja la cabeza
             cout << "  +---+\n"
                     "  |   |\n"
                     "  O   |\n"
@@ -29,7 +43,9 @@ void ihDibujarAhorcado(int vida)
                     "      |\n"
                     "=========\n";
             break;
+
         case 4:
+            // Se añade el torso
             cout << "  +---+\n"
                     "  |   |\n"
                     "  O   |\n"
@@ -38,7 +54,9 @@ void ihDibujarAhorcado(int vida)
                     "      |\n"
                     "=========\n";
             break;
+
         case 3:
+            // Se añade un brazo
             cout << "  +---+\n"
                     "  |   |\n"
                     "  O   |\n"
@@ -47,7 +65,9 @@ void ihDibujarAhorcado(int vida)
                     "      |\n"
                     "=========\n";
             break;
+
         case 2:
+            // Se añaden ambos brazos
             cout << "  +---+\n"
                     "  |   |\n"
                     "  O   |\n"
@@ -56,7 +76,9 @@ void ihDibujarAhorcado(int vida)
                     "      |\n"
                     "=========\n";
             break;
+
         case 1:
+            // Se añade una pierna
             cout << "  +---+\n"
                     "  |   |\n"
                     "  O   |\n"
@@ -65,7 +87,9 @@ void ihDibujarAhorcado(int vida)
                     "      |\n"
                     "=========\n";
             break;
+
         case 0:
+            // Figura completa (ahorcado)
             cout << "  +---+\n"
                     "  |   |\n"
                     "  O   |\n"
